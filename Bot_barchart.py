e6c7b6721088338e709a07d79baf053fba9c4278
JeudyV <46583912+JeudyV@users.noreply.github.com>
2020-03-28 10:46:09 -0600

    update bot barchart and data barchart

---
+++ Bot_barchart.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+import json
+import time
+from telethon.tl.types import PeerChat
+
+import mysql.connector
+from mysql.connector import Error
+
+import traceback
+import os
+
+import re
+
+token = os.getenv('TOKEN_TELEGRAM', '974187013:AAEj2ppqBQySTwdOGIt7EaVaRFGQ7v_KELc')
+url_server = os.getenv('SERVER_URL', 'http://127.0.0.1:5000')
+
+#token = '1078478278:AAHLLs2LJpTFBt19ppcb0_I9kfANo3cb5gA'
+url = 'https://api.telegram.org/bot{}/'.format(token)
+
+def get_updates(offset=None):
+    time.sleep(1)
+    try:
+        URL = url + 'getUpdates'
+        if offset:
+            URL += '?offset={}'.format(offset)
+        res = requests.get(URL)
+        print("Getting: {} elements".format(len(json.loads(res.text)["result"])))
+        
+        if len(json.loads(res.text)["result"]) == 0:
+            
+            get_updates()
+        
+        return res.json()
+
+    except:
+        pass
+
+
+def get_last(data):
+    results = data['result']
+    count = len(results)
+    if count == 0:
+        return None
+    last = count - 1
+    last_update = results[last]
+    return last_update
+
+
+def get_last_id_text(updates):
+    last_update = get_last(updates)
+    if last_update is None:
+        return None, None, None
+    chat_id = last_update['message']['chat']['id']
+    update_id = last_update['update_id']
+    try:
+        text = last_update['message']['text']
+    except:
+        text = ''
+    return chat_id, text, update_id
+
+
+def get_last_date_text():
+    last_msg_time = 0
+    text = ""
+    json_test = get_updates()
+    for key, value in json_test.items():
+        if key == "result":
+            for x in value:
+                for key2, value2 in x.items():
+                    if key2 == "message":
+                        for key3, value3 in value2.items():
+                            if key3 == "date":
+                                last_msg_time = value3
+                            if key3 == "text":
+                                text = value3
+    return last_msg_time, text
+
+
+def reply_markup_maker(data):
+    keyboard = []
+    for i in range(0, len(data), 2):
+        key = []
+        key.append(data[i].title())
+        try:
+            key.append(data[i + 1].title())
+        except:
+            pass
+        keyboard.append(key)
+
+    reply_markup = {"keyboard": keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
+
+
+
+def send_message(chat_id, text, reply_markup=None):
+    print(chat_id)
+    URL = url + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
+    if reply_markup:
+        URL += '&reply_markup={}'.format(reply_markup)
+    res = requests.get(URL)
+    while res.status_code != 200:
+        res = requests.get(URL)
+    print(res.status_code)
+
+
+def start(chat_id):
+    message = 'Do you want to start?'
+    reply_markup = reply_markup_maker(['Start'])
+    send_message(chat_id, message, reply_markup)
+
+    chat_id, text, update_id = get_last_id_text(get_updates())
+    while (text.lower() != 'start'):
+        time.sleep(1)
+        chat_id, text, update_id = get_last_id_text(get_updates(update_id))
+
+    return chat_id, text, update_id
+
+
+def welcome_note(chat_id, commands):
+    text = "Welcome to Test Bot"
+    send_message(chat_id, text)
+    text = 'Select an Option'
+    reply_markup = reply_markup_maker(commands)
+    send_message(chat_id, text, reply_markup)
+
+
+def end(chat_id, text, update_id):
+    message = 'Do you want to end?'
+    reply_markup = reply_markup_maker(['Yes', 'No'])
+    send_message(chat_id, message, reply_markup)
+
+    new_text = text
+    while (text == new_text):
+        time.sleep(1)
+        chat_id, new_text, update_id = get_last_id_text(get_updates(update_id))
+
+    if new_text == 'Yes':
+        return 'y'
+    else:
+        return 'n'
+    
+def test(chat_id, update_id):
+    
+    #-------------------------------------------
+    
+    try:
+        plistas = []
+        
+        from data_barchart import get_Call_And_Put
+        
+        get_list_response = get_Call_And_Put()
+            
+        send_message(chat_id, get_list_response)                        
+        
+    except Exception as error:
+        print(error)
+        traceback.print_exc()
+    
+    print("entro")
+    
+    #-------------------------------------------
+    
+def test_(chat_id, update_id):
+    
+    #-------------------------------------------
+    
+    try:
+        plistas = []
+        
+        from data_barchart import get_volatility
+        
+        get_list_response = get_volatility()
+        
+        send_message(chat_id, get_list_response)                        
+        
+    except Exception as error:
+        print(error)
+        traceback.print_exc()
+    
+    print("entro")
+    
+    #-------------------------------------------
+    
+def _test_(chat_id, update_id):
+    
+    #-------------------------------------------
+    
+    try:
+        plistas = []
+        
+        from data_barchart import get_strikePrice
+        
+        get_list_response = get_strikePrice()
+        
+        send_message(chat_id, get_list_response)                        
+        
+    except Exception as error:
+        print(error)
+        traceback.print_exc()
+    
+    print("entro")
+    
+    #-------------------------------------------
+        
+def menu(chat_id, text, update_id):
+    commands = ['P1', 'P2', 'P3']
+    welcome_note(chat_id, commands)
+
+    while text.lower() == 'start':
+        time.sleep(1)
+        chat_id, text, update_id = get_last_id_text(get_updates(update_id))
+        print("waiting")
+    print(text)
+    while text not in commands:
+        time.sleep(1)
+        chat_id, text, update_id = get_last_id_text(get_updates(update_id))
+        print("waiting 2")
+
+    if text == 'P1':
+        test(chat_id, update_id)
+    elif text == 'P2':
+        test_(chat_id, update_id)
+    elif text == 'P3':
+        _test_(chat_id, update_id)
+        
+    text = 'start' 
+                
+
+def main():
+    try:
+        text = ''
+        print('Started step 1.0')
+        chat_id, text, update_id = get_last_id_text(get_updates())
+        print("Update ID: {}".format(update_id))
+        chat_id, text, update_id = start(chat_id)
+        print('Started step 1.2')
+        
+#         if text is None:
+            
+#             print("main 1")
+#             main()
+
+#         while text.lower() != 'y':
+#             print("while main 2")
+#             time.sleep(1)
+#             text = 'start'
+#             menu(chat_id, text, update_id)
+#             text = 'y'
+#             chat_id, text, update_id = get_last_id_text(get_updates())
+
+        menu(chat_id, text, update_id)
+        
+        print("main 2")
+        main()
+
+                
+    except Error as e:
+        
+        print("Error: ", e)
+
+
+main()
+
--- data_barchart.py
+++ data_barchart.py
@@ -4,38 +4,6 @@
 # In[ ]:
 
 
-def generic_header(name_csv):
-    Cov = pd.read_csv(name_csv)
-    Frame=pd.DataFrame(Cov.values, columns = ["strikePrice"
-                ,"lastPrice"
-                ,"percentFromLast"
-                ,"bidPrice"
-                ,"midpoint"
-                ,"askPrice"
-                ,"priceChange"
-                ,"percentChange"
-                ,"volatility"
-                ,"volume"
-                ,"openInterest"
-                ,"optionType"
-                ,"daysToExpiration"
-                ,"expirationDate"
-                ,"tradeTime"
-                ,"symbolType"])
-    Frame.to_csv(name_csv, sep='\t')
-
-
-# In[ ]:
-
-
-def Hello():
-    v = {"message": "Hello, World!"}
-    return v
-
-
-# In[ ]:
-
-
 import requests
 import json
 import pandas as pd
@@ -43,171 +11,102 @@ import csv
 import os.path
 import time
 
-def allData_Call_And_Put_Company(nCompany):
+
+# In[ ]:
+
+
+def get_all_data_barchart():
     try:
         
-        nCompany = nCompany["symbol"].upper()
+        #nCompany = nCompany["symbol"].upper()
 
         l = []
-        l_ = {}
 
-        url = "https://www.barchart.com/proxies/core-api/v1/options/get?symbol={}&fields=strikePrice%2ClastPrice%2CpercentFromLast%2CbidPrice%2Cmidpoint%2CaskPrice%2CpriceChange%2CpercentChange%2Cvolatility%2Cvolume%2CopenInterest%2CoptionType%2CdaysToExpiration%2CexpirationDate%2CtradeTime%2CsymbolCode%2CsymbolType&groupBy=optionType&expirationDate=nearest&meta=field.shortName%2Cexpirations%2Cfield.description&raw=1".format(nCompany)
+        url = "https://www.barchart.com/proxies/core-api/v1/options/get?symbol=AAPL&fields=strikePrice%2ClastPrice%2CpercentFromLast%2CbidPrice%2Cmidpoint%2CaskPrice%2CpriceChange%2CpercentChange%2Cvolatility%2Cvolume%2CopenInterest%2CoptionType%2CdaysToExpiration%2CexpirationDate%2CtradeTime%2CsymbolCode%2CsymbolType&groupBy=optionType&expirationDate=nearest&meta=field.shortName%2Cexpirations%2Cfield.description&raw=1"
 
         payload = {}
         headers = {
           'authority': 'www.barchart.com',
           'accept': 'application/json',
           'sec-fetch-dest': 'empty',
-          'x-xsrf-token': 'eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0=',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+          'x-xsrf-token': 'eyJpdiI6ImRlcnJET3Z1R2dCUlJoZlZHU2EyT2c9PSIsInZhbHVlIjoieVpxZ2s3ZEE4ZE85TmlJMkpQdkZqWTRnckM5OTlRUmR4MFFOY25kVlwveHphaVROTitSWlpveTZYY24zTVNhSlEiLCJtYWMiOiJmZTM5NDJjMDBiODAwNmYwYzZkOGQ4MWNlNGVhYjk5NjdjNGE2OGE3ZWJhZjg2ZGFjODkxNGUyOGJmMDUyMTk0In0=',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
           'dnt': '1',
           'sec-fetch-site': 'same-origin',
           'sec-fetch-mode': 'cors',
           'referer': 'https://www.barchart.com/stocks/quotes/AAPL/options?moneyness=allRows',
           'accept-language': 'es-ES,es;q=0.9,en;q=0.8,nl;q=0.7',
-          'cookie': '_gcl_au=1.1.1010588658.1582606331; _ga=GA1.2.270392010.1582606332; __gads=ID=72d4fe83c6f1b46e:T=1582606336:S=ALNI_MbAIR-3cW-trYQ0awJLtlLrAI4_Jg; OX_plg=pm; fitracking_2=no; Limelight_HTML_Player_UserId=8254E40B-EDB8-4FEF-BEEB-16C1F441C68A; __qca=P0-247333977-1582910365824; market=eyJpdiI6InRZRnN3Z241RTNiVEpQMllnRCtiamc9PSIsInZhbHVlIjoiaFwvZzN2eEI4N2E1c0YwQlwvWFdRdWVRPT0iLCJtYWMiOiIxMmMyMmRlNWU3ZjJmMTk3OWI2N2I2NzA1Y2FjNzkzYzc0NGIxNzlmNjU1YjMzZTk5NGI3N2EyOGNlZjJkOWYyIn0%3D; _gid=GA1.2.414573225.1584147264; fi_utm=direct%7Cdirect%7C%7C%7C%7C; IC_ViewCounter_www.barchart.com=2; _awl=2.1584148483.0.4-687528a2-5451e6f2ba28d5b02db6c3572a2c5043-6763652d75732d6561737431-5e6c3003-0; XSRF-TOKEN=eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0%3D; laravel_session=eyJpdiI6Ik9uZDFsV1V3SFJcL0JxeWdXS2pqdFVnPT0iLCJ2YWx1ZSI6IkdjaHo2bG5XTXZQeVFSK1hcL3NXRE84c3M5bzlNdk05b1RYNVNCNGZ1VEZBUDRUck9NYzNVc2RvMkJmSUdYc0V3IiwibWFjIjoiMjJkYzhmYmFlY2U2MzJkNzU1MGYzNWM3M2U0ODQwZmQ5NDQ4ZjhkZjIzNTBlYjI2YzQ2ZjYyMmU3Mzc4OWQyOCJ9'
+          'cookie': 'XSRF-TOKEN=eyJpdiI6ImRlcnJET3Z1R2dCUlJoZlZHU2EyT2c9PSIsInZhbHVlIjoieVpxZ2s3ZEE4ZE85TmlJMkpQdkZqWTRnckM5OTlRUmR4MFFOY25kVlwveHphaVROTitSWlpveTZYY24zTVNhSlEiLCJtYWMiOiJmZTM5NDJjMDBiODAwNmYwYzZkOGQ4MWNlNGVhYjk5NjdjNGE2OGE3ZWJhZjg2ZGFjODkxNGUyOGJmMDUyMTk0In0%3D; laravel_session=eyJpdiI6IjVVcW5EU0NSdVlnemNHNndMc3grNXc9PSIsInZhbHVlIjoiWUtNMm91WDVFV1wvM3BQc1RSSUF3YXoxRFlESzEwaTlkSTZjY29RWTE5dmJQeDFjUENcL3RaOXZlVngrNjVpWGtYIiwibWFjIjoiYjIwNmYxNWM4OGZjZDYyMmMzMzQyMjljOTc1MTQyNzM2NzZhNWJhMDA2MmQ5YzUzNDRjNzA5MmJmOWJkMjU3MiJ9; market=eyJpdiI6IllhM24rRXo0MDh3UUoyR3lBOVlPXC9BPT0iLCJ2YWx1ZSI6IjBJUjloWDNHTG82UktsaWNOdThYNFE9PSIsIm1hYyI6IjAxMTQwOTZhNjdhZTU2ZGVlZWMwZTZlOTkyZjY1ZDFkZjA4NThmYTRiZWJiYWQyZmY1ZDVjNjEwMTcwZjM5YzIifQ%3D%3D; IC_ViewCounter_www.barchart.com=1; _gcl_au=1.1.1247577706.1584746654; _ga=GA1.2.705207734.1584746654; _gid=GA1.2.1169490786.1584746654; _gat_UA-2009749-51=1; __gads=ID=2607ea29ff9d5f72:T=1584746656:S=ALNI_Mb52y6JNWa1MuAua3ZJHLV-n_S9CA'
         }
 
         response = requests.request("GET", url, headers=headers, data = payload)
 
-        #print(response.text.encode('utf8'))
-
-
-        for k,v in json.loads(response.text).items():
-            if k == 'data':
-                for a,c in v.items():
-                    for x in c:
-                        del x['raw']
-                        l.append(x)
-                    df = pd.DataFrame(l)
-                    df.to_csv('all_data1.csv', index=False, header=True, mode='a')
-                    time.sleep(1)
-        #generic_header('all_data.csv')
-        for l_x in l:
-            print(l_x)
-            l_.setdefault("item",l_x)
-        print(l_)
-        print(type(l_))
-        #json.dumps(l_)
-            
-        return json.loads(l_)
+        response = json.loads(response.text)
+        
+        return response
     except Exception as error:
         print({"result":False, "message":"Something went wrong, error details: {}".format(error)})
         return False
                     
-#allData_Call_And_Put_Company()
+#get_all_data_barchart()
 
 
 # In[ ]:
 
 
-import requests
-import json
-import pandas as pd
-import csv
-import os.path
-import time
-
-def strike_Price_Call_And_Put_Company(nCompany):
-    
+def get_Call_And_Put():
     try:
-    
-        nCompany = nCompany["symbol"].upper()
+        
+        #nCompany = nCompany["symbol"].upper()
 
         l = []
-        l_strikePrice = []
-        l_volatility = []
-
-        url = "https://www.barchart.com/proxies/core-api/v1/options/get?symbol={}&fields=strikePrice%2ClastPrice%2CpercentFromLast%2CbidPrice%2Cmidpoint%2CaskPrice%2CpriceChange%2CpercentChange%2Cvolatility%2Cvolume%2CopenInterest%2CoptionType%2CdaysToExpiration%2CexpirationDate%2CtradeTime%2CsymbolCode%2CsymbolType&groupBy=optionType&expirationDate=nearest&meta=field.shortName%2Cexpirations%2Cfield.description&raw=1".format(nCompany)
-
-        payload = {}
-        headers = {
-          'authority': 'www.barchart.com',
-          'accept': 'application/json',
-          'sec-fetch-dest': 'empty',
-          'x-xsrf-token': 'eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0=',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-          'dnt': '1',
-          'sec-fetch-site': 'same-origin',
-          'sec-fetch-mode': 'cors',
-          'referer': 'https://www.barchart.com/stocks/quotes/AAPL/options?moneyness=allRows',
-          'accept-language': 'es-ES,es;q=0.9,en;q=0.8,nl;q=0.7',
-          'cookie': '_gcl_au=1.1.1010588658.1582606331; _ga=GA1.2.270392010.1582606332; __gads=ID=72d4fe83c6f1b46e:T=1582606336:S=ALNI_MbAIR-3cW-trYQ0awJLtlLrAI4_Jg; OX_plg=pm; fitracking_2=no; Limelight_HTML_Player_UserId=8254E40B-EDB8-4FEF-BEEB-16C1F441C68A; __qca=P0-247333977-1582910365824; market=eyJpdiI6InRZRnN3Z241RTNiVEpQMllnRCtiamc9PSIsInZhbHVlIjoiaFwvZzN2eEI4N2E1c0YwQlwvWFdRdWVRPT0iLCJtYWMiOiIxMmMyMmRlNWU3ZjJmMTk3OWI2N2I2NzA1Y2FjNzkzYzc0NGIxNzlmNjU1YjMzZTk5NGI3N2EyOGNlZjJkOWYyIn0%3D; _gid=GA1.2.414573225.1584147264; fi_utm=direct%7Cdirect%7C%7C%7C%7C; IC_ViewCounter_www.barchart.com=2; _awl=2.1584148483.0.4-687528a2-5451e6f2ba28d5b02db6c3572a2c5043-6763652d75732d6561737431-5e6c3003-0; XSRF-TOKEN=eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0%3D; laravel_session=eyJpdiI6Ik9uZDFsV1V3SFJcL0JxeWdXS2pqdFVnPT0iLCJ2YWx1ZSI6IkdjaHo2bG5XTXZQeVFSK1hcL3NXRE84c3M5bzlNdk05b1RYNVNCNGZ1VEZBUDRUck9NYzNVc2RvMkJmSUdYc0V3IiwibWFjIjoiMjJkYzhmYmFlY2U2MzJkNzU1MGYzNWM3M2U0ODQwZmQ5NDQ4ZjhkZjIzNTBlYjI2YzQ2ZjYyMmU3Mzc4OWQyOCJ9'
+        
+        response = get_all_data_barchart()
+        
+        df = pd.DataFrame(response["data"])
+        
+        list_Call = df.apply(lambda x: [x.get("Call").get("strikePrice"), x.get("Put").get("strikePrice")], axis=1, result_type="expand")
+        list_Put = df.apply(lambda x: x.get("Put").get("strikePrice"), axis=1, result_type="expand")
+        
+        json_strikePrice = {
+            "list_Call": list_Call,
+            "list_Pat": list_Put
         }
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-
-        #print(response.text.encode('utf8'))
-
-        for k,v in json.loads(response.text).items():
-            if k == 'data':
-                for a,c in v.items():
-                    for x in c:
-                        del x['raw']
-                        l.append(x)
-                        for z,t in x.items():
-                            if z == "strikePrice":
-                                l_strikePrice.append(t)
-
-                    df = pd.DataFrame(l_strikePrice)
-                    df.to_csv('strikePrice_Money_question_4.csv', index=False, header=True, mode='a')
-                
-        return l_strikePrice
+        
+        return list_Call
     except Exception as error:
         print({"result":False, "message":"Something went wrong, error details: {}".format(error)})
         return False
                     
-#strike_Price_Call_And_Put_Company('FB')
+#get_Call_And_Put()
 
 
 # In[ ]:
 
 
-import requests
-import json
-import pandas as pd
-import csv
-import os.path
-import time
-
-def IV_Call_And_Put_Company(nCompany):
+def get_volatility():
     
     try:
     
-        nCompany = nCompany["symbol"].upper()
+        #nCompany = nCompany["symbol"].upper()
 
         l_volatility = []
 
-        url = "https://www.barchart.com/proxies/core-api/v1/options/get?symbol={}&fields=strikePrice%2ClastPrice%2CpercentFromLast%2CbidPrice%2Cmidpoint%2CaskPrice%2CpriceChange%2CpercentChange%2Cvolatility%2Cvolume%2CopenInterest%2CoptionType%2CdaysToExpiration%2CexpirationDate%2CtradeTime%2CsymbolCode%2CsymbolType&groupBy=optionType&expirationDate=nearest&meta=field.shortName%2Cexpirations%2Cfield.description&raw=1".format(nCompany)
+        l = []
+        
+        response = get_all_data_barchart()
+        
+        print(response)
+        print(type(response))
 
-        payload = {}
-        headers = {
-          'authority': 'www.barchart.com',
-          'accept': 'application/json',
-          'sec-fetch-dest': 'empty',
-          'x-xsrf-token': 'eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0=',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-          'dnt': '1',
-          'sec-fetch-site': 'same-origin',
-          'sec-fetch-mode': 'cors',
-          'referer': 'https://www.barchart.com/stocks/quotes/AAPL/options?moneyness=allRows',
-          'accept-language': 'es-ES,es;q=0.9,en;q=0.8,nl;q=0.7',
-          'cookie': '_gcl_au=1.1.1010588658.1582606331; _ga=GA1.2.270392010.1582606332; __gads=ID=72d4fe83c6f1b46e:T=1582606336:S=ALNI_MbAIR-3cW-trYQ0awJLtlLrAI4_Jg; OX_plg=pm; fitracking_2=no; Limelight_HTML_Player_UserId=8254E40B-EDB8-4FEF-BEEB-16C1F441C68A; __qca=P0-247333977-1582910365824; market=eyJpdiI6InRZRnN3Z241RTNiVEpQMllnRCtiamc9PSIsInZhbHVlIjoiaFwvZzN2eEI4N2E1c0YwQlwvWFdRdWVRPT0iLCJtYWMiOiIxMmMyMmRlNWU3ZjJmMTk3OWI2N2I2NzA1Y2FjNzkzYzc0NGIxNzlmNjU1YjMzZTk5NGI3N2EyOGNlZjJkOWYyIn0%3D; _gid=GA1.2.414573225.1584147264; fi_utm=direct%7Cdirect%7C%7C%7C%7C; IC_ViewCounter_www.barchart.com=2; _awl=2.1584148483.0.4-687528a2-5451e6f2ba28d5b02db6c3572a2c5043-6763652d75732d6561737431-5e6c3003-0; XSRF-TOKEN=eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0%3D; laravel_session=eyJpdiI6Ik9uZDFsV1V3SFJcL0JxeWdXS2pqdFVnPT0iLCJ2YWx1ZSI6IkdjaHo2bG5XTXZQeVFSK1hcL3NXRE84c3M5bzlNdk05b1RYNVNCNGZ1VEZBUDRUck9NYzNVc2RvMkJmSUdYc0V3IiwibWFjIjoiMjJkYzhmYmFlY2U2MzJkNzU1MGYzNWM3M2U0ODQwZmQ5NDQ4ZjhkZjIzNTBlYjI2YzQ2ZjYyMmU3Mzc4OWQyOCJ9'
-        }
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-
-        #print(response.text.encode('utf8'))
-
-
-        for k,v in json.loads(response.text).items():
+        for k,v in response.items():
             if k == 'data':
                 for a,c in v.items():
                     for x in c:
                         del x['raw']
                         for z,t in x.items():
                             if z == "strikePrice":  
-                                if t == "200.00":
+                                if t == "215.00":
                                     for x1,x2 in x.items():
                                         if x1 == "volatility":
                                             print(x1, x2)
@@ -222,50 +121,25 @@ def IV_Call_And_Put_Company(nCompany):
         print({"result":False, "message":"Something went wrong, error details: {}".format(error)})
         return False
                     
-#IV_Call_And_Put_Company('FB')
+#get_volatility()
 
 
 # In[ ]:
 
 
-import requests
-import json
-import pandas as pd
-import csv
-import os.path
-import time
-
-def strikePrice_350_Call_And_Put_Company(nCompany):
+def get_strikePrice():
     
     try:
     
-        nCompany = nCompany["symbol"].upper()
+        #nCompany = nCompany["symbol"].upper()
 
         l_all = []
 
-        url = "https://www.barchart.com/proxies/core-api/v1/options/get?symbol={}&fields=strikePrice%2ClastPrice%2CpercentFromLast%2CbidPrice%2Cmidpoint%2CaskPrice%2CpriceChange%2CpercentChange%2Cvolatility%2Cvolume%2CopenInterest%2CoptionType%2CdaysToExpiration%2CexpirationDate%2CtradeTime%2CsymbolCode%2CsymbolType&groupBy=optionType&expirationDate=nearest&meta=field.shortName%2Cexpirations%2Cfield.description&raw=1".format(nCompany)
+        l = []
+        
+        response = get_all_data_barchart()
 
-        payload = {}
-        headers = {
-          'authority': 'www.barchart.com',
-          'accept': 'application/json',
-          'sec-fetch-dest': 'empty',
-          'x-xsrf-token': 'eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0=',
-          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-          'dnt': '1',
-          'sec-fetch-site': 'same-origin',
-          'sec-fetch-mode': 'cors',
-          'referer': 'https://www.barchart.com/stocks/quotes/AAPL/options?moneyness=allRows',
-          'accept-language': 'es-ES,es;q=0.9,en;q=0.8,nl;q=0.7',
-          'cookie': '_gcl_au=1.1.1010588658.1582606331; _ga=GA1.2.270392010.1582606332; __gads=ID=72d4fe83c6f1b46e:T=1582606336:S=ALNI_MbAIR-3cW-trYQ0awJLtlLrAI4_Jg; OX_plg=pm; fitracking_2=no; Limelight_HTML_Player_UserId=8254E40B-EDB8-4FEF-BEEB-16C1F441C68A; __qca=P0-247333977-1582910365824; market=eyJpdiI6InRZRnN3Z241RTNiVEpQMllnRCtiamc9PSIsInZhbHVlIjoiaFwvZzN2eEI4N2E1c0YwQlwvWFdRdWVRPT0iLCJtYWMiOiIxMmMyMmRlNWU3ZjJmMTk3OWI2N2I2NzA1Y2FjNzkzYzc0NGIxNzlmNjU1YjMzZTk5NGI3N2EyOGNlZjJkOWYyIn0%3D; _gid=GA1.2.414573225.1584147264; fi_utm=direct%7Cdirect%7C%7C%7C%7C; IC_ViewCounter_www.barchart.com=2; _awl=2.1584148483.0.4-687528a2-5451e6f2ba28d5b02db6c3572a2c5043-6763652d75732d6561737431-5e6c3003-0; XSRF-TOKEN=eyJpdiI6InlFRHBXSlh5c0xwamFnaXhES1wvQ053PT0iLCJ2YWx1ZSI6Ik5zOVpPT0gwb1dHMTBDZFdNOTNESG1WSUFqbkJpTW1FMmVVOGVteURnSWltNnNhSjNmcFFZZVluVSthaTRCYVUiLCJtYWMiOiIzNjYyNWRlZGIxYjExYzQ0OWExODc0YWVmNjQ0MThmNzMzY2NjYmQ4YjBiOWNiMGNmYTNmYjEyOThlODk5OGNlIn0%3D; laravel_session=eyJpdiI6Ik9uZDFsV1V3SFJcL0JxeWdXS2pqdFVnPT0iLCJ2YWx1ZSI6IkdjaHo2bG5XTXZQeVFSK1hcL3NXRE84c3M5bzlNdk05b1RYNVNCNGZ1VEZBUDRUck9NYzNVc2RvMkJmSUdYc0V3IiwibWFjIjoiMjJkYzhmYmFlY2U2MzJkNzU1MGYzNWM3M2U0ODQwZmQ5NDQ4ZjhkZjIzNTBlYjI2YzQ2ZjYyMmU3Mzc4OWQyOCJ9'
-        }
-
-        response = requests.request("GET", url, headers=headers, data = payload)
-
-        #print(response.text.encode('utf8'))
-
-
-        for k,v in json.loads(response.text).items():
+        for k,v in response.items():
             if k == 'data':
                 for a,c in v.items():
                     for x in c:
@@ -277,14 +151,15 @@ def strikePrice_350_Call_And_Put_Company(nCompany):
 
                     df = pd.DataFrame(l_all)
                     df.to_csv('all_data_strikePrice_350_question_1.csv', index=False, header=True, mode='a')
-        #generic_header('all_data_strikePrice_350_question_1.csv')
+        
+        print(l_all)
     
         return l_all
     except Exception as error:
         print({"result":False, "message":"Something went wrong, error details: {}".format(error)})
         return False
                     
-#strikePrice_350_Call_And_Put_Company('FB')
+get_strikePrice()
 
 
 # In[ ]:
